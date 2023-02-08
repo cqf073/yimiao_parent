3bee6b6f6a7b7fe8508d02af25ad27c0d4075255
cqf073 <1730581444@qq.com>
2023-02-08 21:08:58 +0800

    first commit

---
+++ src/main/java/com/cqf/yimiao_parent/YimiaoParentApplication.java
@@ -0,0 +1,13 @@
+package com.cqf.yimiao_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YimiaoParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YimiaoParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cqf/yimiao_parent/YimiaoParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cqf.yimiao_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YimiaoParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
