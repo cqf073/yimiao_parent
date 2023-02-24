1145ab0770f58dcc1a6c46637386b48ea670dd16
cqf073 <1730581444@qq.com>
2023-02-24 10:54:26 +0800

    完成了数据字典的查询

--- service/service-cmn/src/main/java/com/cqf/yimiao/cmn/controller/Dictcontroller.java
+++ service/service-cmn/src/main/java/com/cqf/yimiao/cmn/controller/Dictcontroller.java
@@ -6,10 +6,7 @@ import com.cqf.yimiao.model.cmn.Dict;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +22,7 @@ import java.util.List;
 @Api(value = "数据字典接口")
 @RestController
 @RequestMapping("/admin/cmn/dict")
+@CrossOrigin
 public class Dictcontroller {
 
     @Autowired
